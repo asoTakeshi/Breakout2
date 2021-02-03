355f577c9fb5d280804e56b7f3f3a1df0b73968b
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-02-03 21:21:00 +0900

    壁を貫通しない　ブロック変更

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [Header("Playerの移動速度")]
+    public float moveSpeed = 0.1f;
+    [Header("Playernの移動範囲の制限値")]
+    public float limitPos = 4.25f;  // 追加。この範囲内でpositionを制限する。
+   
+   
+    
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -19,8 +19,12 @@ public class Controller : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position -= transform.forward * 0.1f;
+            transform.position -= transform.forward * moveSpeed;
 
         }
-    }    
+        //playerの移動範囲を制限
+        float z = Mathf.Clamp(transform.position.z, -limitPos, limitPos);
+        // 制限確認後、位置情報を更新
+        transform.position = new Vector3(transform.position.x, transform.position.y, z);
+    }
 }
