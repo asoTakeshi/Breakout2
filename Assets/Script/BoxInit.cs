59471584b77e108281dfed9ddb822ca66d30ff04
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-02-04 12:08:10 +0900

    ブロックランダム

--- Assets/Script/BoxInit.cs
+++ Assets/Script/BoxInit.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class BoxInit : MonoBehaviour
 {
-    public GameObject boxObjPrefab;
-    public GameObject boxesObj;
+    public GameObject [] boxObjPrefabs;  // 配列に変更します。外部で生成したいプレファブを指定(BoxのPrefabはboxObjPrefabs[0]、HardBoxのPrefabはboxObjPrefabs[1]にアサインする、など)
+    public GameObject boxesObj;  // BoxesオブジェクトはboxesObj変数に格納される。
 
+    //Awake関数内の処理はStartよりも先に実行される。
     void Awake()
     {
+        //Hierarchyタブの中からMasterゲームオブジェクトを探してmasterobjに格納
         GameObject masterObj = GameObject.Find("Master");  //<=====これ
         for (int x = 0; x < 8; x++)
         {
             for (int y = 0; y < 5; y++)
             {
-                GameObject g = Instantiate(boxObjPrefab, boxesObj.transform);
+                //0から配列boxObjPrefabsの要素数-1までの範囲で乱数を作って、それをrandomValueに格納する。要素数が2の場合には、0か1が格納される。
+                int randomValue = Random.Range(0, boxObjPrefabs.Length);
+                //Box、またはHardBoxを複製し、gに格納する(複製してできたオブジェクトは、boxesObjに格納されているBoxesゲームオブジェクトの子オブジェクトとなる)。
+
+                GameObject g = Instantiate(boxObjPrefabs[randomValue], boxesObj.transform);
+                //複製してできたオブジェクトの位置の変更
                 g.transform.position = new Vector3((2f + (1f * y)), 0.4f, (-4.2f + (1.2f * x)));
+                //DestroyerスクリプトのmasterobjにMasterゲームオブジェクトを格納
                 g.GetComponent<Destroyer>().masterObj = masterObj;  //<=====これ
             }
         }
